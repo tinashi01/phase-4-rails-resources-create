f7da39936364f3b2e26ac765f2b985a1d8f482b6
Tina Shi <init.defaultBranch>
2023-06-16 00:24:13 +1000

    commit tree f49e420fa777c236dfa571df3f4ae8b3f6a54cf9

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,10 @@
 class BirdsController < ApplicationController
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -8,12 +13,10 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
+    # params[:id] refers to the dynamic part of our route, defined by :id
+    # a request to /birds/2 would give params[:id] a value of 2
     bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    render json: bird
   end
 
 end
